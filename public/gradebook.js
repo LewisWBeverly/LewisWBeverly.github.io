abc3cd5113e3e464260b2ff9021d81c765c7a3ec
LewisBeverly <lwbeverly@my.waketech.edu>
2025-07-23 19:01:34 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -13,8 +13,7 @@ function fetchGradeData() {
         if(xhr.readyState === xhr.DONE){
             // Check if we're successful
             if(xhr.status !== 200){
-                console.error('Could not get grades.
-                   Status: ${xhr.status}');
+                console.error(`Could not get grades. Status: ${xhr.status}`);
             }
             // And then call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText));
@@ -26,8 +25,8 @@ function fetchGradeData() {
 
 // TODO: Populate the table with grade data
 function populateGradebook(data) {
-     // This function will take the fetched grade data and populate the table
-     console.log("Populate gradebook with data:", data);
+    // This function will take the fetched grade data and populate the table
+    console.log("Populate gradebook with data:", data);
 }
 
 // TODO REMOVE THIS
