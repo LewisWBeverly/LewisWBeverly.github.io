e800c0ee41eb0267d7936e1b579afea66e686f64
LewisBeverly <lwbeverly@my.waketech.edu>
2025-07-24 13:32:15 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,36 +1,30 @@
-// TODO: Fetch data from the PostgreSQL database (to be implemented later)
 function fetchGradeData() {
-    // This function will query the PostgreSQL database and return grade data
     console.log("Fetching grade data...");
-    // Create a new request for HTTP data
     let xhr = new XMLHttpRequest();
-    // This is the address on the machine we're asking for data
     let apiRoute = "/api/grades";
-    // When the request changes status, we run this anonymous function
-    xhr.onreadystatechange = function(){
-        let results;
-        // Check if we're done
-        if(xhr.readyState === xhr.DONE){
-            // Check if we're successful
-            if(xhr.status !== 200){
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status !== 200) {
                 console.error(`Could not get grades. Status: ${xhr.status}`);
+                return; // stop here if there's an error
             }
-            // And then call the function to update the HTML with our data
-            populateGradebook(JSON.parse(xhr.responseText));
+            try {
+                let results = JSON.parse(xhr.responseText);
+                populateGradebook(results);
+            } catch (e) {
+                console.error("Could not parse JSON:", e.message);
+            }
         }
-    }.bind(this);
+    };
     xhr.open("get", apiRoute, true);
     xhr.send();
 }
 
-// TODO: Populate the table with grade data
+// This function will take the fetched grade data and populate the table
 function populateGradebook(data) {
-    // This function will take the fetched grade data and populate the table
     console.log("Populate gradebook with data:", data);
 }
 
-// TODO REMOVE THIS
-// Call the stubs to demonstrate the workflow
-const gradeData = fetchGradeData();
-populateGradebook(gradeData);
-// END REMOVE
+// Just call fetchGradeData to start the process
+fetchGradeData();
